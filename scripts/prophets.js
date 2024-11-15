56d47a8c18703ca39a84d84e0680a5ad6cea6b5b
Cristobal Henriquez <brohenriquez@gmail.com>
2024-11-15 17:02:54 -0500

    Chamber Directory Page-started but incomplete

---
+++ scripts/prophets.js
@@ -0,0 +1,39 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayProphets(data.prophets); // Display prophets array
+        } else {
+            console.error("Failed to fetch prophet data");
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach(prophet => {
+        const card = document.createElement("section");
+        const fullName = document.createElement("h2");
+        const portrait = document.createElement("img");
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
+
+getProphetData();
